0183478e181fc49badb0d579f667c94b7a5afd72
nugriz <rizki@happy5.co>
2023-11-27 18:05:44 +0700

    add login card

--- src/App.vue
+++ src/App.vue
@@ -21,9 +21,33 @@ watchEffect(() => {
 </script>
 
 <template>
-  <meta name="theme-color" content="#FFFFFF" />
+  <!-- header -->
+  <meta name="theme-color" content="#87CEEB" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  
+  <!-- body -->
+  
+  <!-- login & signup -->
+  <div class="card text-bg-light mt-12" style="max-width: 18rem;" :style="{ 'visibility': state.name === 'home' ? 'visible' : 'hidden'}">
+    <div class="card-header"><h1>Log in</h1></div>
+    <div class="card-body">
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label"><h6>Email address</h6></label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label"><h6>Password</h6></label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button type="submit" class="btn btn-primary"><h6>Submit</h6></button>
+      </form>
+    </div>
+  </div>
+
+
   <button :style="{ 'visibility': state.name === 'home' ? 'visible' : 'hidden'}" type="button" class="btn btn-primary" @click="state.count++">
     <i class="bi bi-shop"></i>
   </button>
