03fdbc56cbc196d03e98817967faaf6ae60b6ffd
nugriz <rizki@happy5.co>
2024-03-25 17:57:19 +0700

    change base url

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ const state = reactive({
  pc: false,
  checkout: false,
  messages: [],
- base: 'https://ngestok-8ff9388ae0c0.herokuapp.com'
+ base: 'https://scds-05aa2c4a8a4e.herokuapp.com/'
 //  base: 'http://localhost:3000'
 })
 
